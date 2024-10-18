4c215672bdf8757cca2b93e93270a1586f3d7b8d
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-10-18 10:39:21 -0600

    Delete Ejercicio_34.py

--- Ejercicio_34.py
+++
@@ -1,6 +0,0 @@
-numeros = input("Ingresa números separados por comas: ").split(',')
-numeros = [int(num) for num in numeros]
-     
-pares = len([num for num in numeros if num % 2 == 0])      
-impares = len([num for num in numeros if num % 2 != 0])
-print(f"Números pares: {pares}, Números impares: {impares}")
